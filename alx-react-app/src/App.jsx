55d5a8438993975d6e19518138b4be4c8bfc60d9
Machelli <valpraiz81@gmail.com>
2025-08-31 16:14:45 +0100

    text modified

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,8 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage.jsx'
-import Footer from './Footer.jsx';
-import Header from './Header.jsx';
+import Footer from './Footer';
+import Header from './Header';
 import MainContent from './MainContent';
 
 function App() {
