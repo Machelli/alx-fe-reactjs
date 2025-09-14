e6adb466521d2b08fe09f53c495a106d625cfe67
Machelli <valpraiz81@gmail.com>
2025-09-14 07:33:45 -0700

    userprofile css adjusted

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -4,8 +4,8 @@ const UserProfile = () => {
    const props = {name: 'Praise', bio: "A tech enthusiat with intense passion", age: '37'}
 
    return (
-     <div style={{border: '2px solid green', padding: '20px', margin: '20px'}}>
-       <h2 style={{ color: 'white', backgroundColor: 'green', display: 'inline', borderRadius: '5px'}} >{props.name}</h2>
+     <div style={{border: '2px solid green', padding: '10px', margin: '10px'}}>
+       <h2 style={{ color: 'blue', backgroundColor: 'white', display: 'inline', borderRadius: '5px'}} >{props.name}</h2>
        <p>Age:  <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
        <p>Bio:  <span style={{ fontWeight: 'bold' }}>{props.bio}</span></p>
      </div>
