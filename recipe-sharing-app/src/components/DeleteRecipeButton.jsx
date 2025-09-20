f5a26903c69fcc2bc9c19a744beda967d49018b2
Machelli <valpraiz81@gmail.com>
2025-09-20 00:14:40 -0700

    adjusted

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,17 +1,25 @@
-const DeleteRecipeButton = ({ recipeId, openModal }) => {
+import useNavigate  from "react-router-dom"
+import useRecipeStore from "./recipeStore"
+import deleteRecipe  from "./recipeStore"
+
+const DeleteRecipeButton = ({ recipeId }) => {
+  const navigate = useNavigate();
+  const deleteRecipe = useRecipeStore(state => state.deleteRecipe);
   
   const handleClick = () => {
-    openModal(recipeId);
+    deleteRecipe(recipeId);
+    navigate('/');
   };
 
   return (
     <button
       onClick={handleClick}
-      
     >
       Delete
     </button>
   );
 };
+
+deleteRecipe()
 
 export default DeleteRecipeButton;
