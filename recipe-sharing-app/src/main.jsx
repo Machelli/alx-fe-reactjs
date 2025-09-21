7bacf3819277832400a91cbc43cd51649b9a5086
Machelli <valpraiz81@gmail.com>
2025-09-20 18:00:59 -0700

    try for the second time

--- recipe-sharing-app/src/main.jsx
+++ recipe-sharing-app/src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import RecommendationsList from './components/RecommendationsList.jsx'
+import FavoritesList from './components/FavoritesList.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+<FavoritesList/>
+
+<RecommendationsList />
+
+
