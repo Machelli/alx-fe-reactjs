030e6ccc70da16e011eb42c1b285818a1c1b1ebb
Machelli <valpraiz81@gmail.com>
2025-10-05 11:32:37 +0100

    imaged resized

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,7 +3,7 @@ import './index.css'
 function UserProfile() {
   return (
     <div className="bg-gray-100 max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8  max-w-xs sm:max-w-xs md:max-w-sm " >
-      <img src="https://via.placeholder.com/150" alt="User"  className=' w-24 h-24 sm:w-28 sm:h-28 md:w-36 md:h-36 rounded-full mx-auto'/>
+      <img src="https://via.placeholder.com/150" alt="User"  className=' w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full mx-auto'/>
       <h1 className='text-xl text-blue-800 my-4 mb-1 md:mb-2 text-xl sm:text-lg md:text-xl'>John Doe</h1>
       <p className='text-gray-600 text-base text-sm sm:text-base'>Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
